099e944303d1e22fe054523f1e668a144133acad
Behrad3d <behrad.3d@gmail.com>
2016-07-20 14:10:29 -0400

    Initial Commit

---
+++ SOM_Chart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SOM_Chart
+//
+//  Created by Behrad Bagheri on 7/20/16.
+//  Copyright © 2016 BoringB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
